7aa184b0baf991f719c6f06bff6b0ed5361c7a92
Alexis Guzman <codingcodax@gmail.com>
2023-10-03 15:18:16 -0600

    feat(nvim): custom plugins

---
+++ nvim/user/plugins/user.lua
@@ -0,0 +1,8 @@
+return {
+	-- You can also add new plugins here as well:
+	-- Add plugins, the lazy syntax
+	{ "rktjmp/lush.nvim" },
+	{ "nyoom-engineering/oxocarbon.nvim" },
+	{ "projekt0n/github-nvim-theme" },
+	{ "uloco/bluloco.nvim" },
+}
