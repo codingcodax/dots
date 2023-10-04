207594d759609132b1a5f8ceb38410b39ade6db1
Alexis Guzman <codingcodax@gmail.com>
2023-10-04 13:27:34 -0600

    feat(nvim): remove unnecessary config options

--- nvim/plugins/user.lua
+++ nvim/plugins/user.lua
@@ -1,6 +1,4 @@
 return {
-	-- You can also add new plugins here as well:
-	-- Add plugins, the lazy syntax
 	{ "rktjmp/lush.nvim" },
 	{ "nyoom-engineering/oxocarbon.nvim" },
 	{ "projekt0n/github-nvim-theme" },
